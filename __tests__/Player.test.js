e5866fb023eae67750614222075fe069c6d5274c
Guadalupe Rocha <guadaluperocha@GuadalupesMBP2.attlocal.net>
2022-07-25 17:54:16 -0500

    player object

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,13 +1,12 @@
+const Player = require('../lib/Player');
 const Potion = require('../lib/Potion');
 
 jest.mock('../lib/Potion');
 
-console.log(new Potion());
 
-const Player = require('../lib/Player');
 
 test('creates a player object', () => {
-    const player = new Player ('Dave');
+    const player = new Player('Dave');
 
     expect(player.name).toBe('Dave');
     expect(player.health).toEqual(expect.any(Number));
@@ -17,4 +16,23 @@ test('creates a player object', () => {
     expect(player.inventory).toEqual(
         expect.arrayContaining([expect.any(Object)])
     );
+});
+
+test("gets player's stats as an object", () => {
+    const player = new Player('Dave');
+
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+});
+
+test('gets inventory from player or returns false', () => {
+    const player = new Player('Dave');
+
+    expect(player.getInventory()).toEqual(expect.any(Array));
+
+    player.inventory = [];
+
+    expect(player.getInventory()).toEqual(false);
 });
